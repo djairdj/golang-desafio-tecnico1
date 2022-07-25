123796323c813fee455840b54029535a1937fb0a
Djair Alves <djair.dj@gmail.com>
2022-07-24 22:46:22 -0300

    Correção da localização dos métodos da interface

--- internal/service/product.go
+++ internal/service/product.go
@@ -1,13 +1,1 @@
 package product
-
-import (
-	"context"
-	"github.com/djairdj/golang-desafio-tecnico1/internal/entity"
-)
-
-type Repository interface {
-	Create(ctx context.Context, name string) (*entity.Product, error)
-	List(ctx context.Context) ([]entity.Product, error)
-	GetOne(ctx context.Context, id string) (*entity.Product, error)
-	Update(ctx context.Context, p *entity.Product) error
-}
