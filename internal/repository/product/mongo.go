da2c8a76f9eefb941016b6cedd5dc1b1c1c03622
Djair Alves <djair.dj@gmail.com>
2022-07-24 22:53:21 -0300

    Defininto implementação da Interface por meio de Factory

--- internal/repository/product/mongo.go
+++ internal/repository/product/mongo.go
@@ -1,1 +1,14 @@
 package product
+
+import "go.mongodb.org/mongo-driver/mongo"
+
+type mongoRepository struct {
+	collection *mongo.Collection
+}
+
+func NewMongoRepository(db *mongo.Database) Repository {
+	collectionProduct := db.Collection("product")
+	return &mongoRepository{
+		collection: collectionProduct,
+	}
+}
